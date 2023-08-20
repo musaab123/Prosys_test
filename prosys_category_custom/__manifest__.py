85a6a1e8b03782b8d54b9424c8ce46f8eecb1c3f
musaab123 <67084214+musaab123@users.noreply.github.com>
2023-08-20 19:32:56 +0400

    Add files via upload

---
+++ prosys_category_custom/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Product Category Barcode',
+    'version': '16.0.1.0.0',
+    'category': 'Warehouse',
+    'summary': 'Product Category Barcode',
+    'author': 'prosys',
+    'company': 'prosys',
+    'maintainer': 'prosys',
+    'images': ['static/description/banner.png'],
+    'depends': ['stock','sale_management','purchase','account'],
+    'data': [
+        'views/factory.xml',
+ 
+    ],
+    
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+
+}
