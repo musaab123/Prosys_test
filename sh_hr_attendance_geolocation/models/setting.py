758968d4c854e0de37f50cd9a8f9ab598754ead1
musaab123 <67084214+musaab123@users.noreply.github.com>
2023-09-12 19:00:51 +0400

    Add files via upload

---
+++ sh_hr_attendance_geolocation/models/setting.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = ['res.config.settings']
+
+    distance_id = fields.Float(string='Distance',  config_parameter='sh_hr_attendance_geolocation_distance_id')
