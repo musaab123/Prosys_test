efc35fdfc079f4fb858ab3e7c2f4a46858df647b
musaab123 <67084214+musaab123@users.noreply.github.com>
2023-09-23 13:12:41 +0400

    Add files via upload

---
+++ parcode_dymo_custom/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    NCTR, Nile Center for Technology Research
+#    Copyright (C) 2021-2022 NCTR (<http://www.nctr.sd>).
+#
+##############################################################################
+{
+    "name": "product parcode dymo",
+    "version": "1.1",
+    "category": "Generic Modules/product",
+    "description": """ Manage product module
+
+    """,
+    "author": "Prosys Tech",
+    "depends":['product','stock','account' ,'product_barcode_sequence'],
+    "data": [
+
+        'reports/product_parcode_inherit.xml',
+
+        
+        
+
+    
+    ],
+    "installable": True,
+    "active": True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
