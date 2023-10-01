9459727ad494082e9c253dea232a41bfa543a1f3
musaab123 <67084214+musaab123@users.noreply.github.com>
2023-10-01 18:41:03 +0400

    Add files via upload

---
+++ prosys_restrict_out_of_stock/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': 'Sale Restrict Out Of Stock',
+    'version': '0.0.1',
+    'summary': 'Sale Restrict Out Of Stock',
+    'description': 'Sale Restrict Out Of Stock',
+    'category': 'Category',
+    'author': 'Prosys',
+    'website': 'Website',
+    'license': 'LGPL-3',
+    'depends': ['sale', 'purchase','account'],
+    'data': [
+        # 'security/security.xml',
+        'views/res_configration_view.xml',
+        'views/product_template_view.xml'
+
+        
+
+
+        
+    ],
+        "images": ["static/description/background.png", ],
+
+    'installable': True,
+    'auto_install': False
+}
---
+++ prosys_restrict_out_of_stock/models/res_configration.py
@@ -0,0 +1,125 @@
+from odoo.exceptions import ValidationError
+from odoo.exceptions import UserError
+
+from odoo import api, fields, models, _
+
+class SaleConfiguration(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    restrect_sale = fields.Boolean(string='Restrict Product Sale Without Stock')
+    restrict_sale_user_ids = fields.Many2many('res.users', string='Available User')
+
+    def set_values(self):
+        super(SaleConfiguration, self).set_values()
+        self.env['ir.config_parameter'].sudo().set_param('prosys_restrict_out_of_stock.restrect_sale', self.restrect_sale)
+        self.env['ir.config_parameter'].sudo().set_param('prosys_restrict_out_of_stock.restrict_sale_user_ids', ','.join(map(str, self.restrict_sale_user_ids.ids)))
+
+    def get_values(self):
+        res = super(SaleConfiguration, self).get_values()
+        restrect_sale = self.env['ir.config_parameter'].sudo().get_param('prosys_restrict_out_of_stock.restrect_sale', default=False)
+        restrict_sale_user_ids = self.env['ir.config_parameter'].sudo().get_param('prosys_restrict_out_of_stock.restrict_sale_user_ids', default='').split(',')
+        # Filter out empty strings and convert to integers
+        restrict_sale_user_ids = [int(user_id) for user_id in restrict_sale_user_ids if user_id]
+        res.update(
+            restrect_sale=bool(restrect_sale),
+            restrict_sale_user_ids=[(6, 0, restrict_sale_user_ids)],
+        )
+        return res
+
+
+
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    unavailable_qty = fields.Boolean(string='Sell Unavailable Qty')
+    qty_available = fields.Float(string='Available Quantity', compute='_compute_qty_available')
+
+    @api.depends('product_variant_ids', 'product_variant_ids.qty_available')
+    def _compute_qty_available(self):
+        for template in self:
+            template.qty_available = sum(template.product_variant_ids.mapped('qty_available'))
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+
+    @api.onchange('order_line')
+    def check_margin(self):
+        for order in self:
+            restrect_sale = self.env['ir.config_parameter'].sudo().get_param('prosys_restrict_out_of_stock.restrect_sale', default=False)
+            restrict_sale_user_ids = self.env['ir.config_parameter'].sudo().get_param('prosys_restrict_out_of_stock.restrict_sale_user_ids', default='')
+
+            restrict_sale_user_ids = [int(user_id) for user_id in restrict_sale_user_ids.split(',') if user_id]
+
+            for line in order.order_line:
+                current_user_id = self.env.user.id
+                product_template = line.product_template_id
+                qty_available = product_template.qty_available
+                product_name = product_template.name
+                product_qty = line.product_uom_qty
+
+                if product_template.unavailable_qty:
+                    continue  
+
+                if restrect_sale and current_user_id in restrict_sale_user_ids:
+                    continue 
+
+                if product_qty > qty_available:
+                    raise ValidationError(_("You can't confirm this order because of the following reasons:\n"
+                        "You added ({}) of ({}) but you only have ({}) available in YourCompany Warehouse.\n"
+                        "To confirm this order, please contact the administrator.").format(product_qty, product_name, qty_available))
+
+
+
+
+    # @api.onchange('order_line')
+    # def check_margin(self):
+    #     for order in self:
+    #         restrect_sale = order.env['ir.config_parameter'].sudo().get_param('prosys_restrict_out_of_stock.restrect_sale', default=False)
+    #         restrict_sale_user_ids = order.env['ir.config_parameter'].sudo().get_param('prosys_restrict_out_of_stock.restrict_sale_user_ids', default='')
+
+    #         restrict_sale_user_ids = [int(user_id) for user_id in restrict_sale_user_ids.split(',') if user_id]
+
+    #         print("jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj", restrict_sale_user_ids)
+
+    #         for line in order.order_line:
+    #             if line.product_template_id.unavailable_qty:
+    #                 if restrect_sale:
+    #                     if order.user_id.id not in restrict_sale_user_ids:
+    #                         if line.product_uom_qty > line.product_template_id.qty_available:
+    #                             raise ValidationError(_("You can't confirm this order because of the following reasons:\n"
+    #                     "You added ({}) of ({}) but you only have ({}) available in YourCompany Warehouse.\n"
+    #                     "To confirm this order, please contact the administrator.").format(line.product_uom_qty, line.product_template_id.name, line.product_template_id.qty_available))
+
+    #             elif line.product_uom_qty > line.product_template_id.qty_available:
+    #                         raise ValidationError(_("You can't confirm this order because of the following reasons:\n"
+    #                    "You added ({}) of ({}) but you only have ({}) available in YourCompany Warehouse.\n"
+    #                    "To confirm this order, please contact the administrator.").format(line.product_uom_qty, line.product_template_id.name, line.product_template_id.qty_available))
+
+
+
+
+    @api.depends('user_id')
+    def _compute_restrect_sale(self):
+        for order in self:
+            order.restrect_sale = self.env['res.config.settings'].get_values().get('restrect_sale', False)
+
+    @api.depends('user_id')
+    def _compute_restrict_sale_user_ids(self):
+        for order in self:
+            order.restrict_sale_user_ids = self.env['res.config.settings'].get_values().get('restrict_sale_user_ids', [])
+
+    restrect_sale = fields.Boolean(
+        string='Restrict Product Sale Without Stock',
+        compute='_compute_restrect_sale',
+        store=False
+    )
+
+    restrict_sale_user_ids = fields.Many2many(
+        'res.partner',
+        string='Available User',
+        compute='_compute_restrict_sale_user_ids',
+        store=False
+    )
