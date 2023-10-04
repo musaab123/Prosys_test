a19016488207872855e28c57812573a9e46abf2a
musaab123 <67084214+musaab123@users.noreply.github.com>
2023-10-05 00:01:52 +0400

    Add files via upload

---
+++ prosys_product_season_custom/models/__init__.py
@@ -0,0 +1,4 @@
+from . import hscode
+from . import hscode_sale
+from . import hscode_purchase
+from . import hscode_invoice
---
+++ prosys_product_season_custom/models/hscode_purchase.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2022-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Amaya Aravind EV (<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+
+from odoo import models, fields, api, tools
+
+
+class ProductBrand(models.Model):
+    _inherit = 'product.template'
+
+    season_id = fields.Many2one('product.season', string='Season')
+
+class BrandProduct(models.Model):
+    _name = 'product.season'
+
+    name = fields.Char(String="Name" ,translate=True)
+    brand_image = fields.Binary()
+    member_ids = fields.One2many('product.template', 'season_id')
+    product_count = fields.Char(String='Product Count', compute='get_count_products', store=True)
+
+    @api.depends('member_ids')
+    def get_count_products(self):
+        self.product_count = len(self.member_ids)
+
+
+class PurchaseHsCodePivot(models.Model):
+    _inherit = 'purchase.report'
+
+    season_id = fields.Many2one('product.season', string='Season')
+
+    def _select(self):
+        res = super(PurchaseHsCodePivot, self)._select()
+        query = res.split('t.categ_id as category_id,', 1)
+        rese = query[0] + 't.categ_id as category_id,t.season_id as season_id,' + query[1]
+        return rese
+
+    def _group_by(self):
+        res = super(PurchaseHsCodePivot, self)._group_by()
+        query = res.split('t.categ_id,', 1)
+        res = query[0] + 't.categ_id,t.season_id,' + query[1]
+        return res
