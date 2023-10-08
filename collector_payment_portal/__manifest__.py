fdd84e0b154260251c17af89d7a52c61e1aae437
musaab123 <67084214+musaab123@users.noreply.github.com>
2023-10-08 14:16:45 +0400

    collector payment portal

---
+++ collector_payment_portal/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Colloctor Payment Portal",
+    'version': '16.0.1.2',
+    'summary': """
+        Apply Colloctor Payment from portal.Easily access to Colloctor Payment and apply Colloctor Payment from portal.User can easily  from portal.""",
+    'description': """
+        =>Apply Colloctor Payment from portal.Easily access to expense and apply expense from portal.
+        =>User can easily create expense from portals.
+        =>Showninf old Colloctor Payment lines data.
+    """,
+    'category': 'Accounting/Colloctor Payment',
+    'author': 'Prosys',
+    'company': 'PROSYS',
+    'depends': ['base','account', 'portal', 'account_inherit',],
+    'data': [
+        'views/portal_account_payment_templates.xml',
+        
+    ],
+    'assets': {
+        'web.assets_backend': [
+            '/account_inherit/static/src/**/*',
+            '/account_inherit/static/src/xml/**/*',
+        ],
+        'web.assets_frontend': [
+            '/collector_payment_portal/static/src/js/attendance_portal.js',
+            '/collector_payment_portal/static/src/js/checkin_out.js',
+        ],
+    },
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
