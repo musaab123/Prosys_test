6d20c4f743fbe2e7f225f2b2c4d5dd020236093e
musaab123 <67084214+musaab123@users.noreply.github.com>
2023-10-15 20:55:44 +0400

    Add files via upload

---
+++ prosys_partner_ledger/__manifest__.py
@@ -0,0 +1,39 @@
+{
+    'name': 'Partner Ledger Report',
+    'version': '16.0.0.2',
+    'category': 'Account',
+    'license': 'OPL-1',
+    'summary': 'Allow to print Partner Ledger Report.',
+    'description': """
+    Allow to print Partner Ledger Report.
+    
+    """,
+    'price': 000,
+    'currency': 'EUR',
+    'author': 'Prosys',
+    'website': 'https://www.brosys.com',
+    'depends': ['base','account','account_reports'],
+    'data': [           
+           
+            'views/partner_view.xml',
+            'views/custom_header_footer.xml',
+            'views/footer_custom.xml',
+            'views/custom_footer_template.xml',
+
+
+            
+
+
+
+
+
+
+            
+
+    ],
+    'installable': True,
+    'auto_install': False,
+    'live_test_url':'https://youtu.be/qehLT4WOWPs',
+    "images":["static/description/Banner.gif"],
+}
+
