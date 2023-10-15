a27cc96e2671913a8f025d29af79d0105add53fe
musaab123 <67084214+musaab123@users.noreply.github.com>
2023-10-15 15:14:41 +0400

    Add files via upload

---
+++ evo_portal_task/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Portal User Project Management Access",
+    'summary': """portal user, project management access, project, access""",
+    'description': """portal user project management access
+                    """,
+    'author': "Evozard",
+    'website': "http://evozard.com/",    
+    'category': 'Other',
+    'version': '16.0.0.1',
+    'sequence': 15,
+    'depends': ['project','hr','hr_timesheet', 'portal', 'website'],
+    'license': "AGPL-3",
+    'price': 49.99,
+    'currency': 'USD',
+    'data': [
+        
+        'view/project_portal_tempalte.xml',
+        'view/task_view.xml',
+    ],
+    
+    'images': ['static/description/banner.png'],
+    'auto_install': False,
+    'installable': True,
+    'application':True,
+}
