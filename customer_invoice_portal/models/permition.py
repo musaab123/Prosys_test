41ed49ee322c1e7d302972d86a3646c6ec2ed533
musaab123 <67084214+musaab123@users.noreply.github.com>
2023-10-23 19:07:54 +0400

    remove customer invoice from repo

--- customer_invoice_portal/models/permition.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields, api
-from odoo.tools.misc import format_date, formatLang
-
-     
-class HrEmployee(models.Model):
-    _inherit = 'hr.employee'
-
-    is_customer_invoice = fields.Boolean('Customer Invoice (Portal)')
