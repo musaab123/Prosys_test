e79a7981f98e13db4ba1d1f4fd04777bc92978df
musaab123 <67084214+musaab123@users.noreply.github.com>
2023-10-25 11:31:46 +0400

    add old parcode and evo project

---
+++ prosys_old_barcode/models/product_template.py
@@ -0,0 +1,17 @@
+# Copyright 2021 VentorTech OU
+# Part of Ventor modules. See LICENSE file for full copyright and licensing details.
+
+from odoo import models, fields, api, _
+from odoo.osv import expression
+from odoo.exceptions import UserError
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    barcode_ids = fields.One2many(related='product_variant_ids.barcode_ids', readonly=False)
+
+    @api.onchange('barcode_ids')
+    def _onchange_lines(self):
+        if len(self.barcode_ids) > 1:
+            raise UserError("You can only add one Barcode to this Broduct.")
