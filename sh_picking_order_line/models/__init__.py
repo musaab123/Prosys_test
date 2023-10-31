f8b59f38939b66ad7ba7b2ae59e20ed56fbb7b62
musaab123 <67084214+musaab123@users.noreply.github.com>
2023-10-31 11:29:47 +0400

    morning update sh order line picking v_3 push to repo

---
+++ sh_picking_order_line/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Part of Softhealer Technologies.
+
+from . import stock_model
+from . import stock_move
+from . import crm
+from . import stock_picking
+from . import active_stock_paking
