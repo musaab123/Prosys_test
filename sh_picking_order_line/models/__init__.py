92e9e56d5b900cedaedadbb3cb26b03f57b09fa4
musaab123 <67084214+musaab123@users.noreply.github.com>
2023-11-13 13:16:53 +0400

    remove from github

--- sh_picking_order_line/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Softhealer Technologies.
-
-from . import stock_model
-from . import stock_move
-from . import crm
-from . import stock_picking
-from . import active_stock_paking
