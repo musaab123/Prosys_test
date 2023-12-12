96c59c69b1ff497a4ab2b75b4d5f3656990a097a
musaab123 <67084214+musaab123@users.noreply.github.com>
2023-12-12 13:39:35 +0400

    add partner extra data to github repo

---
+++ posys_partner_extra_info/models/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from . import partner_activte
+from . import res_partner
+from . import stock
+from . import jop_postion
+from . import purchase
+from . import regional_area
+from . import district
