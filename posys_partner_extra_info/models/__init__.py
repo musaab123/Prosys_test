4a4ddcb9d54a9ef60cc9d00e7ce0f6f7c72f45a2
musaab123 <67084214+musaab123@users.noreply.github.com>
2023-12-16 12:50:30 +0400

    remove from github

--- posys_partner_extra_info/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import partner_activte
-from . import res_partner
-from . import stock
-from . import jop_postion
-from . import purchase
-from . import regional_area
-from . import district
