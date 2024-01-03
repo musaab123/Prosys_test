a99d8da05117419944f8d451baa07a68339b3eff
musaab123 <67084214+musaab123@users.noreply.github.com>
2024-01-03 13:33:52 +0400

    delete from github

--- partner_location/models/field_service_setting.py
+++
@@ -1,5 +0,0 @@
-from odoo import models, fields, api, tools
-
-class ResConfigSettings(models.TransientModel):
-    _inherit = ['res.config.settings']
-    distance_id = fields.Float(string='Distance',  config_parameter='partner_location_distance_id')
